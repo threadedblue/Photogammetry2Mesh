7d5ec7ed2b5719b546ebb69dc24e41c4a0c4e9dc
threadedblue <geoffry.roberts@gmail.com>
2022-11-06 09:33:14 -0500

    Initial Commit

--- Photogammetry2Mesh/main.swift
+++ Photogammetry2Mesh/main.swift
@@ -1,11 +1,54 @@
-//
-//  main.swift
-//  Photogammetry2Mesh
-//
-//  Created by Geoffry Roberts on 11/5/22.
-//
-
 import Foundation
+import RealityKit
+import Combine
 
-print("Hello, World!")
+final class Session {
+    
+    let inputFolder = URL(fileURLWithPath: "/Users/myUser/myPictures", isDirectory: true)
+    let outputFile = URL(fileURLWithPath: "/Users/myUser/result.usdz")
+    var subscriber: AnyCancellable?
+    let semaphore = DispatchSemaphore(value: 0)
+    
+    func run() throws {
+        let configuration = PhotogrammetrySession.Configuration()
+        let session = try PhotogrammetrySession(
+            input: inputFolder,
+            configuration: configuration
+        )
+        let request = PhotogrammetrySession.Request.modelFile(url: outputFile)
+        
+        let waiter = Task.init {
+            do {
+                for try await output in session.outputs {
+                    switch output {
+                    case .processingComplete:
+                        print("")
+                    case .requestError(let request, let error):
+                        print("", request, error)
+                    case .requestComplete(let request, let result):
+                        print("", request, result)
+                    case .requestProgress(let request, let fractionComplete):
+                        print("", request, fractionComplete)
+                    case .inputComplete:
+                        print("")
+                    case .invalidSample(let id, let reason):
+                        print("", id, reason)
+                    case .skippedSample(let id):
+                        print("", id)
+                    case .automaticDownsampling:
+                        print("")
+                    case .processingCancelled:
+                        print("")
+                    @unknown default:
+                        print("")
+                    }
+                }
+            } catch {
+                print("Output: ERROR = \(String(describing: error))")
+                // Handle error.
+            }
+        }
+    }
+}
 
+try! Session().run()
